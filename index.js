82cf64480c8c385ee00d21ac2a132598cc19053c
Yana Savchenko <yana.s.savchenko@gmail.com>
2020-05-16 19:31:18 +0300

    Add layouts and views

--- index.js
+++ index.js
@@ -4,27 +4,54 @@ const exphbs = require('express-handlebars');
 
 const app = express();
 
+app.use(express.static('public'));
+
 // Handlebars settings
 
 const hbs = exphbs.create({
-    defaultLayout: 'main',
-    extname: 'hbs',
+  defaultLayout: 'main',
+  extname: 'hbs',
 })
 app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
+
+//routes
+
 app.get('/', (req, res) => {
-    res.render('index');
+  res.render(
+    'index',
+    {
+      title: 'Home',
+      isHome: true,
+    }
+  );
 });
 
-app.get('/about', (req, res) => {
-    res.render('about');
+app.get('/courses', (req, res) => {
+  res.render(
+    'courses',
+    {
+      title: 'Courses',
+      isCourses: true,
+    }
+  );
+});
+
+app.get('/add-course', (req, res) => {
+  res.render(
+    'add-course',
+    {
+      title: 'Add course',
+      isAdd: true,
+    }
+  );
 });
 
 //server
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    `Server is running on port ${PORT}`
+  `Server is running on port ${PORT}`
 })
