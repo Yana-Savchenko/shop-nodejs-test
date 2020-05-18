2ef16631bf4234536a1bb385a5eccf3cada58a24
Yana Savchenko <yana.s.savchenko@gmail.com>
2020-05-18 23:49:25 +0300

    Add feature updating courses

--- routes/courses.js
+++ routes/courses.js
@@ -1,6 +1,6 @@
 const { Router } = require('express');
 
- const Course = require('../models/course');
+const Course = require('../models/course');
 const router = Router();
 
 
@@ -15,5 +15,35 @@ router.get('/', async (req, res) => {
     }
   );
 });
+router.get('/:id', async (req, res) => {
+  const course = await Course.getById(req.params.id);
+  res.render(
+    'course',
+    {
+      layout: 'empty',
+      title: course.title,
+      course
+    }
+  );
+});
+
+router.get('/:id/edit', async (req, res) => {
+  if (!req.query.allow) {
+    return res.redirect('/');
+  }
+  const course = await Course.getById(req.params.id);
+  res.render(
+    'edit-course',
+    {
+      title: `Edit ${course.title}`,
+      course
+    }
+  );
+});
+
+router.post('/edit', async (req, res) => {
+  await Course.update(req.body);
+  res.redirect('/courses');
+})
 
 module.exports = router; 
