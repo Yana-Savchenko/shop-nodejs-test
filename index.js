0c32ec783b26900aa0839bb06f046f6eb2a68ec3
Jana Savchenko <yana.s.savchenko@gmail.com>
2020-05-22 18:01:48 +0300

    Add mongoose

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 const homeRoutes = require('./routes/home');
 const cartRoutes = require('./routes/cart');
 const coursesRoutes = require('./routes/courses');
 const addCourseRoutes = require('./routes/add-course');
 
+dotenv.config();
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -23,7 +26,7 @@ app.set('views', 'views');
 
 //Parse request
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 //routes
 
@@ -32,9 +35,20 @@ app.use('/courses', coursesRoutes);
 app.use('/add-course', addCourseRoutes);
 app.use('/cart', cartRoutes);
 
-//server
+//start server and connect to DB
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  `Server is running on port ${PORT}`
-})
+async function start() {
+  try {
+    const PORT = process.env.PORT || 3000;
+    const url = `mongodb+srv://${process.env.NAME}:${process.env.PASSWORD}@cluster0-qa9lw.mongodb.net/test?retryWrites=true&w=majority`;
+
+    await mongoose.connect(url, { useUnifiedTopology: true, useNewUrlParser: true });
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    })
+  } catch (err) {
+    console.log('error:', err);
+  }
+}
+
+start();
